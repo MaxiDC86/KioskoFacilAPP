139548d046183a43baf81ac479791dc633fae0b0
maxidc86 <max-rub@hotmail.com>
2023-08-10 12:10:41 -0300

    form to create product done

--- screens/createProduct.js
+++ screens/createProduct.js
@@ -1,26 +1,54 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Alert, TextInput } from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Product } from "../model/product";
 import Button from "../UI/Button";
-import { insertProduct } from "../util/database";
+import { insertProduct, deleteProduct } from "../util/database";
 
 function CreateProduct() {
   const navigation = useNavigation();
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredBarCode, setEnteredBarCode] = useState("");
+
+  function changeTitleHandler(enteredTitle) {
+    setEnteredTitle(enteredTitle);
+  }
+  function changePriceHandler(enteredPrice) {
+    setEnteredPrice(enteredPrice);
+  }
+  function changeBarCodeHandler(enteredBarCode) {
+    setEnteredBarCode(enteredBarCode);
+  }
 
   async function crearHandler() {
-    const product = new Product("milanesa", 7500, 88341234, 1);
+    const product = new Product(
+      enteredTitle,
+      parseInt(enteredPrice),
+      parseInt(enteredBarCode)
+    );
 
     await insertProduct(product);
 
     navigation.navigate("PRODUCTOS");
   }
+  async function borrarHandler() {
+    await deleteProduct(1);
+  }
 
   return (
     <View>
-      <Text>Product creation form</Text>
+      <Text>Product name:</Text>
+      <TextInput onChangeText={changeTitleHandler} value={enteredTitle} />
+      <Text>Product Price:</Text>
+      <TextInput onChangeText={changePriceHandler} value={enteredPrice} />
+      <Text>Product Bar Code:</Text>
+      <TextInput onChangeText={changeBarCodeHandler} value={enteredBarCode} />
       <View>
         <Button onPress={crearHandler}>Crear producto.</Button>
+      </View>
+      <View>
+        <Button onPress={borrarHandler}>Borrar producto.</Button>
       </View>
     </View>
   );
