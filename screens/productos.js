cf6a80d611b6e3d9a1384b01c3df11d9c34f9412
maxidc86 <max-rub@hotmail.com>
2023-08-10 00:31:22 -0300

    productos.js creado para mostrar los productos

--- screens/productos.js
+++ screens/productos.js
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { fetchProducts } from "../util/database";
+import { useIsFocused } from "@react-navigation/native";
 
 function Productos() {
+  const [loadedProducts, setLoadedProducts] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function getProducts() {
+      const products = await fetchProducts();
+
+      setLoadedProducts(products);
+    }
+    if (isFocused) {
+      // setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+      getProducts();
+    }
+  }, [isFocused]);
+
   return (
     <View>
-      <Text> Administración !!!!!</Text>
+      <Text> Productos !!!!!</Text>
+      <FlatList
+        data={loadedProducts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text>
+            {item.title}
+            {item.barCode}
+          </Text>
+        )}
+      />
     </View>
   );
 }
