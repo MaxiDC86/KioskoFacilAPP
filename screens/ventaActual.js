950b70b3035a7cd7e7af39e751a24c942a8138c1
maxidc86 <max-rub@hotmail.com>
2023-08-11 13:33:38 -0300

    creacion de carro de compras

--- screens/ventaActual.js
+++ screens/ventaActual.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
 import Button from "../UI/Button";
 import { useNavigation } from "@react-navigation/native";
+import { Product } from "../model/product";
+import { useIsFocused } from "@react-navigation/native";
 
 function VentaActual({ route }) {
   const navigation = useNavigation();
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [totalShoppingCart, setTotalShoppingCart] = useState(0);
+  const isFocused = useIsFocused();
+  const title = route.params?.title;
+  const price = route.params?.price;
+  const barCode = route.params?.barCode;
+  const id = route.params?.id;
 
-  const product = route.params?.product;
+  useEffect(() => {
+    if (isFocused) {
+      setShoppingCart((curProducts) => [
+        ...curProducts,
+        new Product(title, price, barCode, id),
+      ]);
+      setTotalShoppingCart((curValue) => curValue + price);
+    }
+  }, [isFocused]);
 
   function buttonFinalizarHandler() {
+    setShoppingCart([]);
     navigation.navigate("OPERACIONES");
   }
 
@@ -17,9 +35,21 @@ function VentaActual({ route }) {
   }
   return (
     <View>
-      <ScrollView>
-        <Text>{product}</Text>
-      </ScrollView>
+      <FlatList
+        data={shoppingCart}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text style={styles.tag}>
+            {item.title} ${item.price}
+          </Text>
+        )}
+      />
+      <View>
+        <Text style={styles.totalPrice}>
+          Total de esta compra ${totalShoppingCart}
+        </Text>
+      </View>
+
       <View>
         <Button onPress={buttonAgregarHandler}>Agregar producto.</Button>
       </View>
@@ -36,7 +66,19 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
   },
-  tag: {},
+  tag: {
+    padding: 2,
+    margin: 5,
+    fontSize: 16,
+    borderRadius: 8,
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+  totalPrice: {
+    padding: 4,
+    margin: 5,
+    fontSize: 22,
+  },
   logo: {
     width: 150,
     height: 150,
