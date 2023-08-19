4866433af29dd2949179dffc0196f2781b02570c
maxidc86 <max-rub@hotmail.com>
2023-08-18 23:46:21 -0300

    updating the title and price

--- screens/details.js
+++ screens/details.js
@@ -1,31 +1,74 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useIsFocused } from "@react-navigation/native";
 import Button from "../UI/Button";
+
+import { updateProduct } from "../util/database";
 
 function Details({ route }) {
   const navigation = useNavigation();
   const [imagen, setImagen] = useState(
     "https://reactnative.dev/img/tiny_logo.png"
   );
+  const [titleProd, setTitleProd] = useState("");
+  const [priceProd, setPriceProd] = useState("");
+
+  const id = route.params?.product.id;
   const title = route.params?.product.title;
   const price = route.params?.product.price;
   const barCode = route.params?.product.barCode;
   const imageUri = route.params?.product.imageUri;
+
   const isFocused = useIsFocused();
 
   useEffect(() => {
     setImagen(imageUri);
+    setPriceProd(price);
+    setTitleProd(title);
   }, [isFocused]);
 
   function buttonReturnHandler() {
     navigation.navigate("PRODUCTOS");
   }
+  async function buttonUpdateHandler() {
+    if (titleProd === "") {
+      setTitleProd(title);
+    }
+    if (priceProd === "") {
+      setPriceProd(price);
+    }
+    const updateSuccess = await updateProduct(
+      id,
+      titleProd,
+      priceProd,
+      barCode,
+      imageUri
+    );
+    if (updateSuccess) {
+      navigation.navigate("PRODUCTOS");
+    }
+  }
   return (
     <View>
-      <Text style={styles.text}>{title}</Text>
-      <Text style={styles.text}>${price}</Text>
+      <View>
+        <Text style={styles.text}>Actual: {title}</Text>
+        <TextInput
+          style={styles.text}
+          onChangeText={setTitleProd}
+          value={titleProd}
+          placeholder="Nombre nuevo"
+        />
+      </View>
+      <View>
+        <Text style={styles.text}>${price}</Text>
+        <TextInput
+          style={styles.text}
+          value={priceProd}
+          onChangeText={setPriceProd}
+          placeholder="Precio nuevo"
+        />
+      </View>
       <Text style={styles.text}>Codigo de barras: {barCode}</Text>
       <Image
         style={styles.logo}
@@ -33,6 +76,7 @@ function Details({ route }) {
           uri: imagen,
         }}
       />
+      <Button onPress={buttonUpdateHandler}>Guardar modificación.</Button>
       <Button onPress={buttonReturnHandler}>Volver a productos</Button>
     </View>
   );
