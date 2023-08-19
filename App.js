c5de84b6a31c512fa794c4141695bf13e324c16c
maxidc86 <max-rub@hotmail.com>
2023-08-19 00:21:30 -0300

    Status bar background color black

--- App.js
+++ App.js
@@ -91,7 +91,7 @@ export default function App() {
   }
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="light" backgroundColor="black" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
